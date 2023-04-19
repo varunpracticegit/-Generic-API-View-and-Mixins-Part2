a4eae444989a33d11c009752cb0f0773ddbb5af9
varunskillzamp <varunessence25@gmail.com>
2023-04-19 12:28:31 +0530

    Initial Commit

---
+++ GenericAPIView_and_Mixins2/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('stuapi/', views.LCStudentAPI.as_view()),
+    path('stuapi/<int:pk>/', views.RUDStudentAPI.as_view()),
+
+]
---
+++ api/views.py
@@ -0,0 +1,39 @@
+# GenericAPI View and Model Mixin
+
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+
+# List and Create - pk is not required
+
+class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+
+
+# Retrieve, Update and Destroy - pk is required
+
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+       return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+       return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+       return self.destroy(request, *args, **kwargs)
+    
+
+
+ 
